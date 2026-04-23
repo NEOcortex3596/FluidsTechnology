4daf11cb4e11477c7e67ae9fc07977fb296c72e7
HydroLMS <156966857+NEOcortex3596@users.noreply.github.com>
2026-04-24 03:33:42 +0800

    sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mechrofluids-v7'; // Updated for the new Icon assets
+const CACHE_NAME = 'mechrofluids-v8'; // Updated for the new Icon assets
 const urlsToCache = [
   './',
   './index.html',
