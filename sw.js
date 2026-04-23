3f9db83edae7ae74c4a8587d6e859397fa293b04
HydroLMS <156966857+NEOcortex3596@users.noreply.github.com>
2026-04-24 01:19:53 +0800

    sw.js

--- sw.js
+++ sw.js
@@ -1,28 +1,39 @@
-const CACHE_NAME = 'fluids-lms-v4';
+const CACHE_NAME = 'mechrofluids-v5'; // Change this number every time you update index.html
 const urlsToCache = [
   './',
   './index.html',
-  './manifest.json',
-  './icon.png'
+  './manifest.json'
 ];
 
-// Install the service worker and cache the files
+// Install Event: Forces the new worker to take over immediately
 self.addEventListener('install', event => {
+  self.skipWaiting(); 
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
-// Serve cached files when offline
+// Activate Event: Deletes ALL old versions of the app from the user's phone
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+// Fetch Event: Try the network first, then fall back to the offline cache
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        // Return the cached version if found, otherwise fetch from the network
-        return response || fetch(event.request);
-      })
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
